1700f55dbc1db1e5980e39fd1a23ba1b7e8f8b7e
VictorLourenco <50780567+VictorLourencoo@users.noreply.github.com>
2022-12-03 19:20:36 -0300

    Update Servente.js

--- server/Servente.js
+++ server/Servente.js
@@ -1,4 +1,4 @@
-const DisciplinaModel = require('../models/disciplinas.model');
+
 
 let ArrayDisciplinas = []
 
@@ -19,16 +19,6 @@ async function list_Disciplinas(args){
     return res;
 }
 
-async function Deletar_Disciplina(args){
- const res =  await deleteOne({ _id: req.params.id })
- return res
-}
-
-async function Buscar_Disciplina(args){
-    const id = req.params.id;
-    const res =  await findById(id)
-    return res;
-}
 
 module.exports ={
     Criar_Disciplina,
